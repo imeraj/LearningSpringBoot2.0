88a03f603539a3b8fa1ae4078b916bee8200baf1
Meraj <meraj.enigma@gmail.com>
2018-04-28 21:18:47 +0600

    Add ReactiveQueryByExampleExecutor support in CommentRepository
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- imageservice/src/main/java/com/meraj/imageservice/comments/repository/CommentRepository.java
+++ imageservice/src/main/java/com/meraj/imageservice/comments/repository/CommentRepository.java
@@ -1,9 +1,11 @@
 package com.meraj.imageservice.comments.repository;
 
 import com.meraj.imageservice.comments.model.Comment;
+import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
 
-public interface CommentRepository extends ReactiveCrudRepository<Comment, String> {
-    Flux<Comment> findByImageId(String imageId);
+public interface CommentRepository extends ReactiveCrudRepository<Comment, String>,
+        ReactiveQueryByExampleExecutor {
+
 }
+
--- imageservice/src/main/java/com/meraj/imageservice/comments/service/CommentService.java
+++ imageservice/src/main/java/com/meraj/imageservice/comments/service/CommentService.java
@@ -2,6 +2,8 @@ package com.meraj.imageservice.comments.service;
 
 import com.meraj.imageservice.comments.model.Comment;
 import com.meraj.imageservice.comments.repository.CommentRepository;
+import com.meraj.imageservice.images.model.Image;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -15,7 +17,10 @@ public class CommentService {
     }
 
     public Flux<Comment> findAllComments(String imageId) {
-        return commentRepository.findByImageId(imageId);
+        Comment comment = new Comment();
+        comment.setImageId(imageId);
+        Example<Comment> example = Example.of(comment);
+        return commentRepository.findAll(example);
     }
 
     public Mono<Void> createComment(Mono<Comment> newComment) {
