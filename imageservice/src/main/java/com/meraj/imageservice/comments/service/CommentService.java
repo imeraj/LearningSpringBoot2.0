06c68ecff4897db1fed657d249de06503db9e19d
Meraj <meraj.enigma@gmail.com>
2018-04-29 14:14:58 +0600

    Commented code added for drop mongodb data
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- imageservice/src/main/java/com/meraj/imageservice/comments/service/CommentService.java
+++ imageservice/src/main/java/com/meraj/imageservice/comments/service/CommentService.java
@@ -7,8 +7,10 @@ import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Example;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -44,4 +46,11 @@ public class CommentService {
     Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
+
+//    @Bean
+//    CommandLineRunner setUp(MongoOperations operations) {
+//        return args -> {
+//            operations.dropCollection(Comment.class);
+//        };
+//    }
 }
